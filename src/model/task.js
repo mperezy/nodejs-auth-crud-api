a8fa59e678f4458a36679eacfcea70e48eafa416
mperezy <mperezy@unitru.edu.pe>
2019-07-15 18:53:35 -0500

    Only display tasks if the logged user has them

--- src/model/task.js
+++ src/model/task.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const TaskSchema = Schema({
     title: String,
     description: String,
+    userId: String,
     status: {
         type: Boolean,
         default: false
